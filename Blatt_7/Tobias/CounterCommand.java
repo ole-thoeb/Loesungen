940baddb24f6b9b3e92652a1475a522f91321b0c
tobiornottobi <44438257+tobiornottobi@users.noreply.github.com>
2019-06-06 22:08:32 +0200

    Add files via upload

---
+++ Blatt_7/Tobias/CounterCommand.java
@@ -0,0 +1,11 @@
+
+public class CounterCommand implements Runnable {
+	public void run() {
+		for(int i=0;i<10;i++) {
+			try {
+				Thread.sleep(new java.util.Random().nextInt(1000));
+			} catch(Exception e) {}
+			System.out.println(i);
+		}
+	}
+}
